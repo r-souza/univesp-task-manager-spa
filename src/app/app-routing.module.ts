ae976389679ae2d17e1f2ff19f0ed206c48aafc6
Rodrigo de Souza <contato@rodrigosouza.eti.br>
2023-10-29 20:28:28 -0300

    Change default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { BaseComponent } from './auth/base/base.component';
 import { AppNavComponent } from './core/components/app-nav/app-nav.component';
 import { AuthGuard } from './core/guards/auth.guard';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'login', redirectTo: 'auth/login', pathMatch: 'full' }, // redirect to correct Login Route
@@ -25,10 +24,11 @@ const routes: Routes = [
     component: AppNavComponent,
     canActivateChild: [AuthGuard],
     children: [
-      { path: '', pathMatch: 'full', redirectTo: 'home' },
+      { path: '', pathMatch: 'full', redirectTo: 'projects' },
       {
         path: 'home',
-        component: HomeComponent,
+        redirectTo: 'projects',
+        pathMatch: 'full',
       },
       {
         path: 'projects',
